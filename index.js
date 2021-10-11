65da94c6e793353e22892dd4ed45f17e0ed74325
marcos.luciano <marcos.luciano@kognitiv.com>
2021-10-11 09:05:01 +0200

    Feat(users + albums): Connected to real endpoint + pagination logic

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express')
+const axios = require('axios');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+const paginate = (array, first, rows) => {
+    return array.slice(first, rows);
+}
+
+app.get('/users', (req, res) => {
+    const segment = '/users';
+    const { first = 0, rows = 10 } = req.query; 
+    return axios.get(`${baseURL}${segment}`)
+        .then(function (response) {
+            return res.send({
+                users: paginate(response.data, first, rows),
+                pages: Math.ceil(response.data.length / rows),
+            })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+})
+
+app.get('/albums', (req, res) => {
+    const segment = '/albums';
+    const { first = 0, rows = 10 } = req.query; 
+    return axios.get(`${baseURL}${segment}`)
+        .then(function (response) {
+            return res.send({
+                albums: paginate(response.data, first, rows),
+                pages: Math.ceil(response.data.length / rows),
+            })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
 })
 
 app.listen(port, () => {
